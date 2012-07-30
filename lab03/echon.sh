08975fdbf0b95ba34899f5ab58c0c746d572f29e
Patrick Teen <patrick.teen@weblance.com.au>
2012-07-30 13:46:45 +1000

    added error checking to lab03

--- lab03/echon.sh
+++ lab03/echon.sh
@@ -1,9 +1,22 @@
-#!/bin/bash
+#!/bin/sh
+
+# check for correct num args
+if [ $# -ne 2 ]
+then
+	echo "Usage: $0"
+	exit
+fi
+
+# check for valid args
+if [ $1 -lt 0  ]
+then
+	echo "$0: argument 1 must be a non-negative integer"
+	exit
+fi
+
 counter=0
 while [ $counter -lt $1 ] 
 do
 	echo $2
 	counter=$((counter+1))
 done
-
-
