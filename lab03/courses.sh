2a281754c0eaec38fc83ae584645162e42531a45
Patrick Teen <patrick.teen@weblance.com.au>
2012-08-03 12:54:48 +1000

    started course scrape

---
+++ lab03/courses.sh
@@ -0,0 +1,20 @@
+#/bin/sh
+
+# pages to download
+# http://www.handbook.unsw.edu.au/vbook2011/brCoursesByAtoZ.jsp?StudyLevel=Undergraduate&descr=#
+# http://www.handbook.unsw.edu.au/vbook2012/brCoursesByAtoZ.jsp?StudyLevel=Postgraduate&descr=#
+
+urlUndergrad="http://www.handbook.unsw.edu.au/vbook2011/brCoursesByAtoZ.jsp?StudyLevel=Undergraduate&descr="`echo $1 | tr a-z A-Z | cut -c1`
+urlPostgrad="http://www.handbook.unsw.edu.au/vbook2011/brCoursesByAtoZ.jsp?StudyLevel=Postgraduate&descr="`echo $1 | tr a-z A-Z | cut -c1`
+
+# set -x
+
+# grab the data
+
+underGrad=`wget -qO- $urlUndergrad | egrep -o "<TD class=\"(evenTableCell)?\"><A href=\"http://www.handbook\.unsw\.edu\.au/undergraduate/courses/201[0-9]/$1[0-9]{4}\.html\">.+</A></TD>"`
+
+# filter the data
+
+echo $underGrad
+
+
