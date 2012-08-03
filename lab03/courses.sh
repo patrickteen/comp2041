c2cee7bd00d3856a756b4ee78921c9d6b0dfec7d
Patrick Teen <patrick.teen@weblance.com.au>
2012-08-03 20:06:21 +1000

    fixed vbook error

--- lab03/courses.sh
+++ lab03/courses.sh
@@ -5,7 +5,7 @@
 # http://www.handbook.unsw.edu.au/vbook2012/brCoursesByAtoZ.jsp?StudyLevel=Postgraduate&descr=#
 
 urlUndergrad="http://www.handbook.unsw.edu.au/vbook2011/brCoursesByAtoZ.jsp?StudyLevel=Undergraduate&descr="`echo $1 | tr a-z A-Z | cut -c1`
-urlPostgrad="http://www.handbook.unsw.edu.au/vbook2011/brCoursesByAtoZ.jsp?StudyLevel=Postgraduate&descr="`echo $1 | tr a-z A-Z | cut -c1`
+urlPostgrad="http://www.handbook.unsw.edu.au/vbook2012/brCoursesByAtoZ.jsp?StudyLevel=Postgraduate&descr="`echo $1 | tr a-z A-Z | cut -c1`
 
 #set -x
 
