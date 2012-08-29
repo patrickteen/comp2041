b653afc84030e2c06f72ffe1bcd2a94384468f07
Patrick Teen <patrick.teen@connect.com.au>
2012-08-30 09:40:55 +1000

    added recursive prereqs

--- lab06/prereq.pl
+++ lab06/prereq.pl
@@ -1,22 +1,50 @@
 #!/usr/bin/perl -w
 
-$url = "http://www.handbook.unsw.edu.au/postgraduate/courses/2012/$ARGV[0].html";
-open F, "wget -q -O- $url|" or die $!;
-while ($line = <F>) {
-	if ($line =~ /.*Prerequisite:.*/) {
-		@matches = $line =~ /[A-Z]{4}[0-9]{4}/gi;
-		for $match (@matches) {
-			print "$match\n";
+my @prereqs;
+my @visited;
+
+my $resursive;
+my $base_course;
+my $last = "";
+
+for $arg (@ARGV) {
+	if ($arg =~ "-r") {
+		$recursive = 1;
+	} else {
+		&get_prereqs($arg)
+	}
+}
+
+for $prereq (sort(@prereqs)) {
+	print "$prereq\n" if ($prereq ne $last); 	#there are duplicates in the array, hack so they aren't printed
+	$last = $prereq;
+}
+
+sub get_prereqs {
+	my @matches;
+	$url1 = "http://www.handbook.unsw.edu.au/undergraduate/courses/2012/$_[0].html";
+	$url2 = "http://www.handbook.unsw.edu.au/postgraduate/courses/2012/$_[0].html";
+	open my $F, "wget -q -O- $url1 $url2|" or die $!;
+	while ($line = <$F>) {
+		if ($line =~ /.*Prerequisite:.*/) {
+			@matches = $line =~ /[A-Z]{4}[0-9]{4}/gi;
+			for $match (@matches) {
+				if (!is_seen($match)) {
+					push(@visited, $match);
+					push(@prereqs, $match);
+					&get_prereqs($match) if $recursive;
+				}
+			}
 		}
 	}
 }
-$url = "http://www.handbook.unsw.edu.au/undergraduate/courses/2012/$ARGV[0].html";
-open F, "wget -q -O- $url|" or die $!;
-while ($line = <F>) {
-	if ($line =~ /.*Prerequisite:.*/) {
-		@matches = $line =~ /[A-Z]{4}[0-9]{4}/gi;
-		for $match (@matches) {
-			print "$match\n";
+
+sub is_seen {
+	my $found;
+	for $val (@visited) {
+		if ($val eq $_[0]) {
+			$found = 1;
 		}
 	}
+	return $found;
 }
